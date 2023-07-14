d737e50454248b6c138326a4dce1f6a0bbb5a1ec
Arunava <arunavapatra18@gmail.com>
2023-07-14 23:49:32 +0530

    Reverting back to not include commandLineRunner. It was a meant for testing. If needed will be push with env variables.

--- src/main/java/dev/arunava/taskmanager/Task/Manager/TaskManagerApplication.java
+++ src/main/java/dev/arunava/taskmanager/Task/Manager/TaskManagerApplication.java
@@ -1,12 +1,7 @@
 package dev.arunava.taskmanager.Task.Manager;
 
-import dev.arunava.taskmanager.Task.Manager.model.User;
-import dev.arunava.taskmanager.Task.Manager.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TaskManagerApplication {
@@ -15,10 +10,4 @@ public class TaskManagerApplication {
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner commandLineRunner(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-		return args -> {
-			userRepository.save(new User("Test User", "testuser","test@user.com",passwordEncoder.encode("password"),"ROLE_USER"));
-		};
-	}
 }
